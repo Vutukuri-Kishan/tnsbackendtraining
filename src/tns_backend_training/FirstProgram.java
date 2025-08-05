3e484eec74e90697842e752bd5c1019d0313d987
KishanVutukuri <133990019+KishanVutukuri@users.noreply.github.com>
2025-08-05 20:27:20 +0530

    Initial commit

---
+++ src/tns_backend_training/FirstProgram.java
@@ -0,0 +1,11 @@
+package tns_backend_training;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		String message = "This is the first program being pushed to GitHub from Eclipse";
+		System.out.println(message);
+
+	}
+
+}
