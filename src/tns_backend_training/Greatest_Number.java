3bcf63c70c65b9169565ae102fbdd056a35c0c8f
KishanVutukuri <133990019+KishanVutukuri@users.noreply.github.com>
2025-08-11 20:03:20 +0530

    Conditional Statement

---
+++ src/tns_backend_training/Greatest_Number.java
@@ -0,0 +1,19 @@
+// program to find the greatest number among three numbers using conditional statements
+
+package tns_backend_training;
+
+public class Greatest_Number {
+     public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 15;
+
+        if (a >= b && a >= c) {
+            System.out.println("a is greatest: ");
+        } else if (b >= a && b >= c) {
+            System.out.println("b is greatest: ");
+        } else {
+            System.out.println("c is greatest: ");
+        }
+    }
+}
